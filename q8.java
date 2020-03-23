f68690171054dc814438308b43d3f4852dfc7fe0
romilbalar <32012049+romilbalar@users.noreply.github.com>
2020-03-23 21:25:57 +0530

    Create q8.java

---
+++ q8.java
@@ -0,0 +1,78 @@
+package com.example.sdcardapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1;
+    Button read, write, clear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        e1 = (EditText)findViewById(R.id.edit1);
+        write = (Button)findViewById((R.id.b1));
+        read = (Button)findViewById(R.id.b2);
+        clear = (Button)findViewById(R.id.b3);
+
+        write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = e1.getText().toString();
+                try{
+                    File f = new File("/sdcard/myfile.txt");
+                    f.createNewFile();
+                    FileOutputStream fo = new FileOutputStream(f);
+                    fo.write(msg.getBytes());
+                    fo.close();
+                    Toast.makeText(getBaseContext(), "Data wrote to SD Card", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        read.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String message;
+                String buf = "";
+                try {
+                    File f = new File("/sdcard/myfile.txt");
+                    FileInputStream fin = new FileInputStream(f);
+                    BufferedReader br = new BufferedReader(new InputStreamReader(fin));
+                    while((message = br.readLine()) != null) {
+                        buf += message;
+                    }
+                    e1.setText(buf);
+                    br.close();
+                    fin.close();
+                    Toast.makeText(getBaseContext(),"Data Recived from SDCARD",Toast.LENGTH_LONG).show();
+                } catch(Exception e) {
+                    Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                e1.setText("");
+            }
+        });
+    }
+}
